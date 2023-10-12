7a9e1bdea44ed2717fc0fa4f7e9e4000eb7a20bf
ejy55n <ejy55n2@gmail.com>
2023-10-12 14:12:37 +0900

    feat: useEffect start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import Button from './Button';
 import styles from './App.module.css';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log('render');
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <h1 className={styles.title}>{counter}</h1>
       <Button text={'Continue'} />
+      <button onClick={onClick}> click me</button>
     </div>
   );
 }
